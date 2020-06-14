2c6de2d571c22cc739eed61d0ec8c70cc809d49b
DESKTOP-KAHICT3\86135 <wangjianwen777@163.com>
2020-06-14 22:29:38 +0800

    no message

--- server/app/router/user.js
+++
@@ -1,6 +0,0 @@
-const router = require("../router");
-
-module.exports=app=>{
-    const {router,controller} = app;
-    router.post('/getEmergencypassportList', controller.user.getMmergencypassportList);
-}
